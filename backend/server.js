95973e9c6cf427a5035d0937d9e8f653d2f0e625
Priya-995 <priya9899nk@gmail.com>
2026-03-20 18:20:28 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
+
 const connectDB=async()=>{
     try{
         await mongoose.connect(MONGO_URI);
